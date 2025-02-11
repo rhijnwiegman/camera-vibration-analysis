6da006ce6350d2a115e518fb4d88d524d30695a0
Rhijn <rhijn.wiegman@student.uva.nl>
2025-02-11 13:40:10 +0100

    modify opencv view stream compatible with picamera2

--- opencv-view-stream.py
+++ opencv-view-stream.py
@@ -1,14 +1,15 @@
 import cv2
+from picamera2 import Picamera2
 
-cap = cv2.VideoCapture(0)
-output = cv2.VideoWriter( 
-        '/home/rhijn/Camera-Stream/output2.avi' , cv2.VideoWriter_fourcc(*'XVID'), 120, (640, 480)) 
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}, controls={"FrameRate": 120}))
+picam2.start()
+#output = cv2.VideoWriter( '/home/rhijn/Camera-Stream/output2.avi' , cv2.VideoWriter_fourcc(*'XVID'), 120, (640, 480)) 
 
 n_frames = 120 * 10
 
 while(n_frames):
-    ret, frame = cap.read()
-    print(ret)
+    frame = picam2.capture_array()
     #output.write(frame) 
     cv2.imshow('frame',frame)
     n_frames -= 1
